4df45f820a8d57bda57f0974559ccbf7a6e71b32
faqih2sib <faqih24si@mahasiswa.pcr.ac.id>
2025-10-02 18:16:48 +0700

    updatept4

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 
-
+//saya faqih
 
 Route::get('/', function () {
     return view('welcome');
